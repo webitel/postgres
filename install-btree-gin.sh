0b932d916d7dd460f46643214abc24c92f97fd98
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2019-09-23 16:49:47 +0300

    btree_gin

--- install-btree-gin.sh
+++ install-btree-gin.sh
@@ -2,5 +2,5 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
-create extension intarray with schema pg_catalog;
+create extension "btree_gin" with schema pg_catalog;
 EOSQL
