3df4d0c4996f2afc2c4c35cd324dd4db12cbb7d5
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-01-22 16:52:24 +0200

    install-wbt-cc updated path

--- install-wbt-cc.sh
+++ install-wbt-cc.sh
@@ -4,11 +4,11 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
 
 CREATE or replace FUNCTION cc_destination_in(cstring) RETURNS cc_destination
-     AS '$libdir/wbt_cc_sql.so', 'cc_destination_in'
+     AS '/usr/local/lib/postgresql/wbt_cc_sql.so', 'cc_destination_in'
      LANGUAGE C IMMUTABLE STRICT;
 
 CREATE or replace FUNCTION cc_destination_in(int4, int4, int8, int4, int4) RETURNS cc_destination
-     AS '$libdir/wbt_cc_sql.so', 'cc_destination_in_args'
+     AS '/usr/local/lib/postgresql/wbt_cc_sql.so', 'cc_destination_in_args'
      LANGUAGE C IMMUTABLE CALLED ON NULL INPUT ;
 
 CREATE TYPE cc_destination (
