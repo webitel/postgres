6a69e1d463c5f798b41f48e0a3e769a5ac74c849
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-01-23 15:14:41 +0200

    update calandar view

--- install-webitel-db.sh
+++ install-webitel-db.sh
@@ -4,18 +4,18 @@ psql --username "$POSTGRES_USER" "$POSTGRES_DB" -a -f /sql/webitel.sql
 psql --username "$POSTGRES_USER" "$POSTGRES_DB" -a -f /sql/tbl_version.sql
 
 psql --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
-insert into calendar_timezone_offsets (id, utc_offset, names)
+insert into call_center.calendar_timezone_offsets (id, utc_offset, names)
 select rank() over(order by utc_offset) id, utc_offset, array_agg(name) as names
 from pg_timezone_names
-where not name ilike '%pos%' and not exists(select 1 from calendar_timezone_offsets)
+where not name ilike '%pos%' and not exists(select 1 from call_center.calendar_timezone_offsets)
 group by utc_offset
 order by utc_offset;
 
-insert into calendar_timezones (name, utc_offset, offset_id)
+insert into call_center.calendar_timezones (name, utc_offset, offset_id)
 select n, utc_offset, id
-from calendar_timezone_offsets,
+from call_center.calendar_timezone_offsets,
      lateral unnest(names) n
-where not exists(select 1 from calendar_timezones);
+where not exists(select 1 from call_center.calendar_timezones);
 
-refresh materialized view calendar_intervals;
+refresh materialized view call_center.calendar_intervals;
 EOSQL
